42dc104012318db9361e9ae70c9cf0de4149f2f7
fhutzchx <fahut.ittiwat@gmail.com>
2020-12-06 22:51:45 +0700

    kuay

--- js/app.js
+++ js/app.js
@@ -9,24 +9,40 @@ $(function() {
           getRecommend();
         }else if (page.id ==="search1"){
           getEntertainment();
-        }else if(page.id ==="profile"){
+        }else if(page.id==="details"){
+          getproductDetail();
+        }
+        else if(page.id ==="profile"){
           getProfile();
         }
     });
+
     document.querySelector('ons-tabbar').addEventListener('reactive', function (event) {
       if (event.index == 0) {
           document.querySelector('#Navigator_home').popPage();
       } else if (event.index == 1) {
           document.querySelector('#Navigator_search1').popPage();
-      }
+      }else if (event.index == 2 ) {
+        document.querySelector('#Navigator_cart').popPage();
+    }
+    else if (event.index == 3 ) {
+      document.querySelector('#Navigator_profile').popPage();
+  }
     })
 });
+
+ function goBack() {
+    document.querySelector('#menu').close().then(function() {
+      document.querySelector('#myNavigator').popPage();
+    });
+  }
+
 
 function getAdvertising(){
     db.collection("advertising").get().then(function (querySnapshot) {
       querySnapshot.forEach(function (doc) {
         const Result = `
-      <ons-carousel-item style="text-align: center;" id="${doc.data().name}"> 
+      <ons-carousel-item style="text-align: center;" > 
       <img src="${doc.data().posterURL}" style=" width:100%; ">
       </ons-carousel-item>`
 
@@ -34,6 +50,7 @@ function getAdvertising(){
       });
     });
   }
+
 
 function getRecommend(){
     db.collection("product").get().then(function (querySnapshot) {
@@ -43,25 +60,21 @@ function getRecommend(){
         <div class="col-6 p-1">
                <div class="photoproduct"> 
                     <div class="d-flex align-items-end">
-                      <img src="${doc.data().posterURL}" style= "width:100%">
+                      <img src="${doc.data().posterURL}" class="productimg" style= "width:100%">
                     </div>
                </div>
         </div> `
 
      $("#recommend").append(Result);
     });
+
+    $(".productimg").click(function () {
+      const productTarget = $(this).attr('id');
+      getproductDetail(productTarget)
+      document.querySelector("#myNavigator").pushPage("views/details.html");
+    });
   });
 }
-
-function openproductDetails(id) {
-    document.querySelector('#myNavigator').pushPage('product_details.html', {data: {title: id}});
-  }
-
-  function goBack() {
-    document.querySelector('#menu').close().then(function() {
-      document.querySelector('#myNavigator').popPage()
-    });
-  }
 
 
   // Search
@@ -113,7 +126,7 @@ function openproductDetails(id) {
       }
 
         const result = `
-        <ons-row class="rowmagin se" id="${doc.data().name}"> 
+        <ons-row class="rowmagin se" > 
         <ons-row style="margin: 5px;">
             <ons-col class="text-center">
             <img src="${doc.data().posterURL}" width="75%" style="margin: 5px 5px;" alt="">
@@ -130,9 +143,10 @@ function openproductDetails(id) {
       }
     });
     $(".se").click(function () {
-      product_detials($(this).attr('id'))
-      document.querySelector("#myNavigator_search").pushPage('view/product_details.html');
-    })
+      const productTarget = $(this).attr('id');
+      getproductDetail(productTarget)
+      document.querySelector("#Navigator_search").pushPage("views/details.html");
+    });
   });
 
 }
@@ -147,27 +161,56 @@ function getEntertainment() {
           querySnapshot.forEach(function (doc) {
               const Entertainment = doc.data().entertainment
               if (Entertainment.includes(targetEntertainment)) {
-                  const result =
-                      /*html*/
-                      `
-
+                  const result = `
                       <ons-col class="col-6 p-1">
                       <div class="containerH" >
-                      <img src="${doc.data().posterURL}" width="100%">
+                      <img src="${doc.data().posterURL}" class="productimg" width="100%">
                    </div>
                       </ons-col>
                       `
                   $("#searchItem").append(result);
               }
           });
-          $(".imgSrc").click(function () {
+          $(".productimg").click(function () {
               const productTarget = $(this).attr('id');
-              getmovieDetailSrc(productTarget)
-              document.querySelector("#Navigator_search").pushPage("views/product_details.html");
+              getproductDetail(productTarget)
+              document.querySelector("#Navigator_search").pushPage("views/details.html");
           });
       });
   })
 }
+function getproductDetail() {
+  db.collection("product").get().then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+
+          
+        
+              const Result = `
+            <ons-carousel class="carousel" swipeable auto-scroll id="${doc.data().name}">
+                <ons-carousel-item>
+                    <img src="${doc.data().posterURL}">
+                </ons-carousel-item>
+            </ons-carousel>
+          
+          `
+              $("#productdetail").append(Result)
+      
+      }); 
+  });
+}
+
+document.addEventListener('init', function (event) {
+  var page = event.target;
+
+  if (page.id === 'page1') {
+      page.querySelector('#push-button').onclick = function () {
+          document.querySelector('#myNavigator').pushPage('index.html', { data: { title: 'Page 2' } });
+      };
+  } else if (page.id === 'page2') {
+      page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  }
+});
+
 
 // Tab
 window.fn = {};
